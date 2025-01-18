f2d51acdc3505688e1ed09c83c8d7f5bd77acbab
Barochkin_MS <Barochkin_MS@teleformis.ru>
2025-01-18 09:42:49 +0300

    docker compose

--- tests/test-all.bash
+++ tests/test-all.bash
@@ -1,5 +1,8 @@
+#RUN TESTS:
+#./tests/test-all.bash start stop
+
 : ${HOST=localhost}
-: ${PORT=7700}
+: ${PORT=8080}
 : ${PRODUCT_ID=1}
 
 function assertCurl() {
@@ -34,10 +37,59 @@ function assertEqual() {
   fi
 }
 
+function testUrl() {
+  url=$@
+  if curl $url -ks -f -o /dev/null
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function waitForService() {
+  url=$@
+  echo -n "Wait for: $url"
+  n=0
+  until testUrl $url
+  do
+    n=$((n + 1))
+    if [ $n = 100 ]
+    then
+      echo "Give up"
+      exit 1
+    else
+      sleep 3
+      echo -n ",retry #$n "
+    fi
+  done
+  echo "DONE, continues..."
+}
+
 set -e
 
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 
+if [[ $@ = *"start"* ]]
+then
+  echo "Restarting test environment..."
+  echo "$ docker compose down --remove-orphans"
+  docker compose down --remove-orphans
+  echo "$ docker compose up -d"
+  docker compose up -d
+fi
+
+waitForService curl http://$HOST:$PORT/product-composite/$PRODUCT_ID
+
 assertCurl 200  "curl http://$HOST:$PORT/product-composite/$PRODUCT_ID -s"
 assertEqual "1" "${RESPONSE:(6):(1)}"
+
+if [[ $@ = *"stop"* ]]
+then
+  echo "Stopping test environment..."
+  echo "$ docker compose down"
+  docker compose down
+fi
+
+echo "End, all tests: OK"
