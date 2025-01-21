52a0a8803d1280fe8d88338a6b4371b953ef3a65
Barochkin_MS <Barochkin_MS@teleformis.ru>
2025-01-21 22:30:28 +0300

    openapi

--- mutual/api/src/main/java/com/unewej/mutual/api/core/composite/product/ProductCompositeService.java
+++ mutual/api/src/main/java/com/unewej/mutual/api/core/composite/product/ProductCompositeService.java
@@ -1,9 +1,14 @@
 package com.unewej.mutual.api.core.composite.product;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "ProductComposite", description = "REST Api for ProductComposite.")
 @RestController
 public interface ProductCompositeService {
     /**
@@ -11,6 +16,13 @@ public interface ProductCompositeService {
      * @param id product id
      * @return product or throw not found
      */
+    @Operation(summary = "${api.productComposite.getProductComposite.description}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+            @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+            @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+            @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
     @GetMapping(value = "/product-composite/{id}", produces = "application/json")
     ProductAggregate getProduct(@PathVariable int id);
 }
